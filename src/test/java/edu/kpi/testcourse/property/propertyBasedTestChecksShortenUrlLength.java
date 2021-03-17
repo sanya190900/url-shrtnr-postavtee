066a301e8686245eda225d89d12e5d13f12c19b1
sanya190900 <sanya190900@gmail.com>
2021-03-17 14:24:47 +0200

    Add property-based tests for creating users and check length of Url

---
+++ src/test/java/edu/kpi/testcourse/property/propertyBasedTestChecksShortenUrlLength.java
@@ -0,0 +1,29 @@
+package edu.kpi.testcourse.property;
+
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.SourceDSL.integers;
+import static org.quicktheories.generators.SourceDSL.strings;
+
+import edu.kpi.testcourse.logic.UrlAndUserActions;
+import edu.kpi.testcourse.model.Url;
+import org.junit.jupiter.api.Test;
+
+public class propertyBasedTestChecksShortenUrlLength {
+
+  /**
+   * Test checks that length of shorten Url should be less than length of full Url
+   */
+  @Test
+  void shortenUrlShouldBeShorterThanFull_propertyBased(){
+    qt()
+      .withExamples(100)
+      .forAll(
+        strings().basicLatinAlphabet().ofLengthBetween(15, 20),
+        integers().between(1, 11)
+      ).check((fullUrl, id) -> {
+      String shortUrl = Url.toShort(id);
+      UrlAndUserActions.saveUrl(shortUrl, fullUrl);
+      return shortUrl.length() < fullUrl.length();
+    });
+  }
+}
---
+++ src/test/java/edu/kpi/testcourse/property/propertyBasedTestForCreatingUsers.java
@@ -0,0 +1,38 @@
+package edu.kpi.testcourse.property;
+
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.SourceDSL.strings;
+
+import edu.kpi.testcourse.bigtable.BigTableImpl;
+import edu.kpi.testcourse.logic.UrlAndUserActions;
+import edu.kpi.testcourse.model.User;
+import java.util.ArrayList;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+public class propertyBasedTestForCreatingUsers {
+  /**
+   * Test checks that after creating random user we can find him so everything works fine
+   */
+  @Test
+  void shouldCreateUser_propertyBased(){
+    qt()
+      .withExamples(100)
+      .forAll(
+        strings().basicLatinAlphabet().ofLengthBetween(7, 20),
+        strings().basicLatinAlphabet().ofLengthBetween(8, 16)
+      ).check((email, password) -> {
+      email = beautifulEmail(email);
+      User user = new User(email, password, new ArrayList<>());
+      user.setIdUser(UUID.randomUUID().toString());
+      UrlAndUserActions.createUser(user);
+      System.out.println(BigTableImpl.dbUsers.size());
+      return UrlAndUserActions.findUserByEmail(email) != null;
+    });
+  }
+
+  private String beautifulEmail(String email){
+    return email + "@gmail.com";
+  }
+}
+
